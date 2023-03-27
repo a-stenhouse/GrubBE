07b4460de426a10bc97937a7c323be91cb10f44c
James Milmine <jamesmilmine@mac.com>
2023-03-27 18:33:18 +0100

    add salts and hashed passwords to dev user data

--- db/data/development/userData.js
+++ db/data/development/userData.js
@@ -1,7 +1,9 @@
 module.exports = [
   {
     username: "John34",
-    password: "Bananas1995",
+    password:
+      "6f5dd1655e97bf25bc1e213946f1d44b872b3d120ef60df267770138395d7b9a", //"Bananas1995",
+    salt: "5b62029b595ee89916c04dd77d104665",
     location: {
       latitude: 52.916668, // Derby
       longitude: -1.466667,
@@ -10,7 +12,9 @@ module.exports = [
   },
   {
     username: "Mike20",
-    password: "Manchester2021",
+    password:
+      "7afb75179a49b52b5464a6b4dc1e0a6f33f7b1eed3c88cc2d4456c78809ed8f0", //"Manchester2021"
+    salt: "8bcecc94d1394ff74dcd1bbcf0cd2aa5",
     location: {
       latitude: 53.39254, // Warrington
       longitude: -2.58024,
@@ -19,7 +23,9 @@ module.exports = [
   },
   {
     username: "Janet876",
-    password: "Lancaster21",
+    password:
+      "dffe4b88d705ab1518171786f4199105e0099aa62621613d6707f7d3b0c09e4c", //"Lancaster21"
+    salt: "cf1ecce6f08d5f583500d3aea18c494b",
     location: {
       latitude: 50.805832, // Portsmouth
       longitude: -1.087222,
@@ -28,7 +34,9 @@ module.exports = [
   },
   {
     username: "Domo",
-    password: "omod123",
+    password:
+      "21a514a09cfefa6482c10bd1323d3f4c4e7fdf61a3085db7740272f7bc757b57", //"omod123"
+    salt: "d5bca30d8b308b732d580da64f1e443a",
     location: {
       latitude: 55.953251, // Edimburgh
       longitude: -3.188267,
@@ -37,7 +45,9 @@ module.exports = [
   },
   {
     username: "RudysPizza",
-    password: "2023RudysEx990",
+    password:
+      "b7da6a5c9044c3a23709f10cf289ab33a51101a410e9be609bbe094a967608bd", //"2023RudysEx990"
+    salt: "8e34bdbceae715e1e435da887e3de9fa",
     location: {
       latitude: 50.716667, // Exeter
       longitude: -3.533333,
@@ -46,7 +56,9 @@ module.exports = [
   },
   {
     username: "CoopEccleshalRd",
-    password: "sheFField1885632",
+    password:
+      "70c9c0802531b3162f64bf484624a009592f033ed2665899c4fb17e1252d52d2", //"sheFField1885632"
+    salt: "841a58e19ce0547852c33e584ef8ebbf",
     location: {
       latitude: 51.380001, // Bath
       longitude: -2.36,
@@ -55,7 +67,9 @@ module.exports = [
   },
   {
     username: "SainsburyLocal",
-    password: "b2bLiverpool2023",
+    password:
+      "245580f3147e5b6218ba10204f245b3a2ffe8dc18236437191a5587bd51543e3", //"b2bLiverpool2023"
+    salt: "fef382ff8e547b9333bc33df34f97cff",
     location: {
       latitude: 53.400002,
       longitude: -2.983333, //Liverpool
@@ -64,7 +78,9 @@ module.exports = [
   },
   {
     username: "FoodLover2334",
-    password: "33256FoodIsLove123",
+    password:
+      "e40c268928cd15df5232ce3ca40f2e9dfc90d335e63e7d74b5ce4cf7c2fc4dcd", //"33256FoodIsLove123"
+    salt: "5b79558047bac0a092843cac215084bb",
     location: {
       latitude: 53.3985, // Liverpool
       longitude: -2.96886,
@@ -73,7 +89,9 @@ module.exports = [
   },
   {
     username: "PlanetSaver12",
-    password: "iloveplanetearth12",
+    password:
+      "46aae4705c206c0b198c73614c737961f1411d1e23c80a4f63a6a7bc07563b15", //"iloveplanetearth12"
+    salt: "8a77559e09f3640dbf75bcd379f91323",
     location: {
       latitude: 53.399259, // Liverpool
       longitude: -2.98392,
@@ -82,7 +100,9 @@ module.exports = [
   },
   {
     username: "HungryToad",
-    password: "Toadlikesfood23",
+    password:
+      "c0f1b594ffe96ad56a1a2959cd8e3a635f249562fecc3d5001dc91585d3b5d41", //"Toadlikesfood23"
+    salt: "6a1052573c1ad4b8c443ebec501c6006",
     location: {
       latitude: 53.40043, // Liverpool
       longitude: -2.978,
--- db/seed/passwordHasher.js
+++ db/seed/passwordHasher.js
@@ -1,14 +1,15 @@
-const crypto = require("crypto")
-const users = require("../data/test/userData.js");
+const crypto = require("crypto");
+const users = require("../data/development/userData.js");
 
 function passwordHasher(password) {
-    const salt = crypto.randomBytes(16).toString("hex");
-    const hashedPassword = crypto.pbkdf2Sync(password, salt, 310000, 32, "sha256").toString("hex");
-    console.log(salt, hashedPassword);
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hashedPassword = crypto
+    .pbkdf2Sync(password, salt, 310000, 32, "sha256")
+    .toString("hex");
+  return { salt, hashedPassword };
 }
 
-// users.forEach((user) => {
-//     passwordHasher(user.password)
-// })
-
-passwordHasher("Mango1998");
+users.forEach((user) => {
+  const { salt, hashedPassword } = passwordHasher(user.password);
+  console.log(`${user.username} salt: ${salt} pass: ${hashedPassword}`);
+});
