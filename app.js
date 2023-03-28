986a51d9de53cd55fc36449f8629772a7cf53a47
James Milmine <jamesmilmine@mac.com>
2023-03-28 22:43:18 +0100

    add /api/auth endpoint and jwt token/local authentication middleware

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+
+require("./passport");
 
 const { postItem } = require("./controllers/itemsControllers");
 const {
@@ -8,7 +12,7 @@ const {
   handle500Errors,
 } = require("./controllers/errorControllers.js");
 
-const { getItems } = require("./controllers/itemsControllers")
+const { getItems } = require("./controllers/itemsControllers");
 const {
   getUsers,
   getUser,
@@ -19,16 +23,35 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/api/auth", (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).send({ msg: info.message });
+    }
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        res.send(err);
+      }
+
+      const token = jwt.sign(user, "your_jwt_secret", { expiresIn: "7d" });
+      return res.status(200).send({ user, token });
+    });
+  })(req, res);
+});
+
 app.get("/api/users/:username", getUser);
 
 app.get("/api/users", getUsers);
 
 app.post("/api/users", postUser);
 
-app.get("/api/items", getItems);
+app.get(
+  "/api/items",
+  passport.authenticate("jwt", { session: false }),
+  getItems
+);
 
 app.post("/api/items", postItem);
-
 
 app.use(customErrors);
 app.use(mongooseErrors);
