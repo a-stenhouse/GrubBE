ce7355cf14bddb8731560bd906de93330cd881f8
Arran <iarranxo@gmail.com>
2023-03-29 15:07:33 +0100

    configured connection for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const ENV = process.env.NODE_ENV || "test";
+const ENV = process.env.NODE_ENV || "test" || "dev";
 require("dotenv").config({
   path: `${__dirname}/../.env.mongoURI.${ENV}`,
 });
